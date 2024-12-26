3bd959f3c2aa7d0fa12118ee698e2da7af840571
anuragverma108 <anuragvermacoding@gmail.com>
2024-12-26 12:34:11 +0530

    Input, Output & Functions in Java

--- IOF.java
+++ IOF.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class IOF {
+
+    public static void greet(){
+        System.out.println("Good Morning");
+    
+    }
+    
+    public static int sum(int a , int b){
+        int sum = a+b;
+         return sum;
+    }
     public static void main(String[] args){
         
-        // taking input
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter your name");
@@ -11,5 +21,26 @@ public class IOF {
         System.out.println("My name is "+ name);
 
 
+         System.out.println("Enter your Address:");
+         String address = sc.nextLine(); 
+ 
+         System.out.println("Enter your age:");
+         int age = sc.nextInt(); 
+         System.out.println("Enter your height:");
+         double height = sc.nextDouble(); 
+ 
+         System.out.println("Are you a student? (true/false):");
+         boolean isStudent = sc.nextBoolean(); 
+ 
+       
+         System.out.println("Name: " + name);
+         System.out.println("Address: " + address);
+         System.out.println("Age: " + age);
+         System.out.println("Height: " + height);
+         System.out.println("Is a Student: " + isStudent);
+ 
+        greet();
+        int s = sum(10,20);
+        System.out.println(s);
     }
 }
