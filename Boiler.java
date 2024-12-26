94b9d5c6566c0237ad6f2663c7075b0cbe30da7e
anuragverma108 <anuragvermacoding@gmail.com>
2024-12-26 11:50:26 +0530

    first commit

---
+++ Boiler.java
@@ -0,0 +1,10 @@
+public class Boiler {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+
+
+
+
+    }
+}
---
+++ IOF.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IOF {
+    public static void main(String[] args){
+        
+        // taking input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("My name is "+ name);
+
+
+    }
+}
